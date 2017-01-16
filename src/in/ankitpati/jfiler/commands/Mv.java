0186744ed63d205fa1ab8f746ef657821d0e6e18
Ankit Pati <ankitpati@gmail.com>
2017-01-16 06:11:01 +0530

    mv: inheriting from cp class

--- src/in/ankitpati/jfiler/commands/Mv.java
+++ src/in/ankitpati/jfiler/commands/Mv.java
@@ -4,32 +4,16 @@ import java.io.*;
 import java.util.*;
 import java.nio.file.*;
 
-public class Mv {
-    ArrayList<String> files;
-
+public class Mv extends Cp {
     public Mv(ArrayList<String> files) throws IllegalArgumentException {
-        if (files.size() != 2)
-            throw new IllegalArgumentException(
-                                            "Usage:\n\tmv <source> <target>");
-
-        this.files = files;
+        super(files);
     }
 
-    public void run() throws IOException {
-        Path source = Paths.get(files.get(0)).toAbsolutePath(),
-             target = Paths.get(files.get(1)).toAbsolutePath();
+    protected void incorrectArguments() {
+        throw new IllegalArgumentException("Usage:\n\tmv <source> <target>");
+    }
 
-        if (!Files.exists(source, LinkOption.NOFOLLOW_LINKS))
-            throw new FileNotFoundException(files.get(0) +
-                                                " (No such file or directory)");
-
-        if (!Files.exists(target.getParent(), LinkOption.NOFOLLOW_LINKS))
-            Files.createDirectories(target.getParent());
-
-        if (Files.isDirectory(target, LinkOption.NOFOLLOW_LINKS))
-            target = Paths.get(target.toString(),
-                                            source.getFileName().toString());
-
+    protected void action(Path source, Path target) throws IOException {
         Files.move(source, target, StandardCopyOption.REPLACE_EXISTING);
     }
 };
