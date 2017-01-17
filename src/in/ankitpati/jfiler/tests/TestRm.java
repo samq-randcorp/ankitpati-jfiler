e5fc7dce29eb6eb609fb33f7763c6a4c2614ed4e
Ankit Pati <ankitpati@gmail.com>
2017-01-17 15:04:16 +0530

    rm: test added

---
+++ src/in/ankitpati/jfiler/tests/TestRm.java
@@ -0,0 +1,44 @@
+package in.ankitpati.jfiler.tests;
+
+import java.io.*;
+import java.util.*;
+import org.testng.*;
+import org.testng.annotations.*;
+import in.ankitpati.jfiler.commands.*;
+
+public class TestRm {
+    ArrayList<String> files = new ArrayList<String>();
+
+    @Test(expectedExceptions = NullPointerException.class)
+    public void testNullArgument() {
+        new Rm(null);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testEmptyArgument() {
+        new Rm(files);
+    }
+
+    @Test
+    public void testValidArgument() throws IOException {
+        files.add("test/touch0.txt");
+        new Rm(files).run();
+        Assert.assertEquals(new File(files.get(0)).exists(), false);
+    }
+
+    @Test
+    public void testMultipleValidArguments() throws IOException {
+        files.add("test/touch1.txt");
+        files.add("test/touch2.txt");
+        new Rm(files).run();
+        for (String file : files)
+            Assert.assertEquals(new File(file).exists(), false);
+    }
+
+    @Test
+    public void testDirectoryArgument() throws IOException {
+        files.add("test/touch/");
+        new Rm(files).run();
+        Assert.assertEquals(new File(files.get(0)).exists(), false);
+    }
+};
