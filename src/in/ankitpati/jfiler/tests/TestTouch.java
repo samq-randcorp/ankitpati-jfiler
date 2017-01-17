6fc7ff98294695eb9d973f42cf83bb3ee9856cc7
Ankit Pati <ankitpati@gmail.com>
2017-01-17 14:09:46 +0530

    touch: test updated to use test directory for temp files

--- src/in/ankitpati/jfiler/tests/TestTouch.java
+++ src/in/ankitpati/jfiler/tests/TestTouch.java
@@ -7,7 +7,7 @@ import org.testng.annotations.*;
 import in.ankitpati.jfiler.commands.*;
 
 public class TestTouch {
-    ArrayList<String> files;
+    ArrayList<String> files = new ArrayList<String>();
 
     @Test(expectedExceptions = NullPointerException.class)
     public void testNullArgument() {
@@ -16,14 +16,22 @@ public class TestTouch {
 
     @Test(expectedExceptions = IllegalArgumentException.class)
     public void testEmptyArgument() {
-        files = new ArrayList<String>();
         new Touch(files);
     }
 
     @Test
     public void testValidArgument() throws IOException {
-        files.add("out/test.txt");
+        files.add("test/touch0.txt");
         new Touch(files).run();
         Assert.assertEquals(new File(files.get(0)).exists(), true);
+    }
+
+    @Test
+    public void testMultipleValidArguments() throws IOException {
+        files.add("test/touch1.txt");
+        files.add("test/touch2.txt");
+        new Touch(files).run();
+        for (String file : files)
+            Assert.assertEquals(new File(file).exists(), true);
     }
 };
